d6f531f4a2619ec457c2e7fdc7be0964bddf8bea
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-13 02:47:09 -0700

    video-19 create a view

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def display(request):
+    return HttpResponse("<h1>My First Django APP!!</h1>")
